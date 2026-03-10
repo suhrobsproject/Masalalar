d5e48433f49662949e41cb70fba9a0474b5fb5e2
suhrobsproject <suhrobsprojects98@gmail.com>
2026-03-10 23:34:46 +0500

    1 chi masalalar
    Qudrat Abdurahimov C++ kitobidan

---
+++ homeworklesson1.py
@@ -0,0 +1,81 @@
+# masala 1
+
+a = int(input('Kvadrat tomonini kiriting: '))
+
+P = 4*a
+
+print("Kvadratning perimetri: ", P)
+
+
+# masala 2
+
+a = int(input("Kvadratning tomonini kiriting: "))
+
+S = a*a
+
+print('Kvadratning yuzasi: ', S)
+
+
+
+# masala 3
+
+a = int(input("a - tomonini kiriting: "))
+b = int(input("b - tomonini kiriting: "))
+
+P = 2 * (a + b)
+S = a * b
+
+print(f"To'rtburchakning yuzi: {S},\nUning perimetri: {P}")
+
+
+# masala 4
+
+d = int(input('Diametrni kiriting: '))
+p = 3.14
+
+L = p * d
+
+print("Aylananing uzunligi: ", L)
+
+
+# masala 5
+
+a = int(input('Kubning tomonini kiriting:'))
+
+V = a**3
+S = 6 * a**2
+
+print(f"Kubning hajmi: {V} \nTo'la sirti: {S} ")
+
+
+#masala 6
+a = int(input("a - tomonini kiriting: "))
+b = int(input("b - tomonini kiriting: "))
+c = int(input("c - tomonini kiriting: "))
+
+V = a * b * c
+S = 2 * (a*b + b*c + a*c)
+
+print(f"Paralelepepidning hajmi: {V} \nTo'la sirti: {S}")
+
+
+#masala 7
+
+r = int(input('radiusini kiriting: '))
+p = 3.14
+L = 2 * p * r
+S = p * r**2
+
+print(f"Doiraning uzunligi: {L} \nUning yuzasi: {S}")
+
+
+#masala 8
+
+a = int(input('1 - sonni kiriting: '))
+b = int(input('2 - sonni kiriting: '))
+
+O = (a + b)/2
+
+print("Sonlarning o'rta afimetigi: ", O)
+
+
